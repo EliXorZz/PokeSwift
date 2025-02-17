bf598580d5abc282cde4ecef1733f786a2d56315
E-milien <emilien.roukine@gmail.com>
2025-02-17 09:05:45 +0100

    changes

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,15 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Emilien Roukine on 17/02/2025.
+//
+
+import SwiftUI
+
+struct Pokemon {
+    let name: String
+    let imageURL: String
+    let types: [String]
+    let stats: [String: Int]
+}
